aa6122cea69a1b3df1ffc1b17c392caed9c2a0e6
Rajveergoud <rajgoud1446@gmail.com>
2025-05-05 00:29:47 +0530

    Day 1 Signing Out

---
+++ script/app.js
@@ -0,0 +1,5 @@
+let from_image = document.querySelector("#from-image");
+let to_image = document.querySelector("#to-image");
+let from_select = document.querySelector("#from");
+let to_select = document.querySelector("#to");
+
